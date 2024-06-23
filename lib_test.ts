06d7fe4313edf2f4011e813cacbf5fca7be7a1aa
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-06-23 19:13:04 +0800

    chore: vargs deno

---
+++ lib_test.ts
@@ -0,0 +1,15 @@
+import { assertEquals } from "jsr:@std/assert";
+import Vargs from "./lib.ts";
+
+Deno.test(function vargTest() {
+  const args = new Vargs([1, 2, () => {
+    console.log("gg");
+  }]);
+  assertEquals(args.length, 3);
+  assertEquals(args.length, 3);
+  assertEquals(args.callbackGiven, false);
+  const args2 = new Vargs([1, 2, () => {
+    console.log("gg");
+  }], () => {});
+  assertEquals(args2.callbackGiven, true);
+});
